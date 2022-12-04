7ff9c0d90521660c27f3ff045ee537c1726692c8
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-04 16:01:33 +0530

    viewcars

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -10,4 +10,8 @@ export class ApiService {
   AddCarsData=(addCarsData:any)=>{
     return this.http.post('https://638c306ad2fc4a058a531f7c.mockapi.io/addCars',addCarsData)
   }
+  fetchCarData=()=>{
+    return this.http.get('https://638c306ad2fc4a058a531f7c.mockapi.io/addCars')
+  }
+
 }
